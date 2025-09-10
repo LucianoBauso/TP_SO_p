2d311d6cb210e263483aa8ff556fae9f51dbf862
Raul Sequeira <raulsequeira@frba.utn.edu.ar>
2025-09-10 22:54:24 +0000

    Implementación inicial de Query Control: conexión al Master
    Se desarrolla la primera versión del módulo Query Control con foco en la conexión:
    - Se agrega main con logger y carga de configuración (IP, PUERTO, CLAVE).
    - Se establece la conexión con el Master utilizando la función crear_conexion.
    - Se implementan funciones auxiliares iniciar_logger, iniciar_config y terminar_programa.
    - Se asegura la liberación correcta de recursos (logger, config y conexión).
    - Queda pendiente el envío de queries y manejo de respuestas.

--- query_control/src/main.c
+++ query_control/src/main.c
@@ -1,6 +1,80 @@
-#include <utils/hello.h>
+#include "main.h"
 
-int main(int argc, char* argv[]) {
-    saludar("query_control");
-    return 0;
+int main(void)
+{
+	/*---------------------------------------------------PARTE 2-------------------------------------------------------------*/
+
+	int conexion;
+	char* ip;
+	char* puerto;
+	char* valor;
+
+	t_log* logger;
+	t_config* config;
+
+	/* ---------------- LOGGING ---------------- */
+
+	logger = iniciar_logger();
+
+	log_info(logger, "Iniciando modulo Query Control !");
+
+
+	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
+
+	config = iniciar_config();
+
+	// Usando el config creado previamente, leemos los valores del config y los 
+	// dejamos en las variables 'ip', 'puerto' y 'valor'
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor = config_get_string_value(config, "CLAVE");
+
+	// Loggeamos el valor de config
+	log_info(logger, "Configuracion: IP=%s PUERTO=%s VALOR=%s", ip, puerto, valor);
+
+	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
+
+	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
+
+	// Creamos una conexión hacia el servidor
+	conexion = crear_conexion(ip, puerto, "Query Control");
+
+	terminar_programa(conexion, logger, config);
+
+	printf("\nQUERY CONTROL CERRADO!!!!!");
+}
+
+t_log* iniciar_logger(void)
+{
+	t_log* nuevo_logger;
+
+	nuevo_logger = log_create("client.log", "CL_LOG", 1, LOG_LEVEL_INFO);
+
+	if(nuevo_logger == NULL){
+		perror("No se pudo crear el logger");
+		exit(EXIT_FAILURE);
+	}
+
+	return nuevo_logger;
+}
+
+t_config* iniciar_config(void)
+{
+	t_config* nuevo_config = config_create("queryControl.config");
+
+	if(nuevo_config == NULL){
+		perror("No se pudo crear el config");
+		exit(EXIT_FAILURE);
+	}
+
+	return nuevo_config;
+}
+
+void terminar_programa(int conexion, t_log* logger, t_config* config)
+{
+	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
+	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	log_destroy(logger);
+	config_destroy(config);
+	liberar_conexion(conexion);
 }
