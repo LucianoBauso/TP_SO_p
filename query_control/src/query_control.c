57962e8218ff56756f333d6024072d9dec2b9d9c
SabrinaRabaglia <sabrinarabaglia@gmail.com>
2025-10-09 23:43:47 +0000

    agrego handshake con master

--- query_control/src/query_control.c
+++ query_control/src/query_control.c
@@ -6,6 +6,10 @@ void ejecutar_query_control(t_argumentos* args, t_config* config, t_log* logger)
     // 1. Conectar al Master
     int conexion = conectar_a_master(config, logger);
     
+    //1.5 --> avisarle a master el tipo de cliente (0 para QC)
+    int tipo = 0;
+    send(conexion, &tipo, sizeof(tipo), 0);
+
     // 2. Enviar solicitud de query
     enviar_solicitud_query(conexion, args, logger);
     
