99acd9e3b7bc3fc72a3506cc7423c155264ddc33
SabrinaRabaglia <sabrinarabaglia@gmail.com>
2025-10-14 23:34:05 +0000

    agrego envio tipo cliente con master

--- worker/src/crear_conexion.c
+++ worker/src/crear_conexion.c
@@ -31,10 +31,9 @@ void crear_client_worker_storage(){
 
 }
 
-void crear_client_worker_master(){
-    char puerto_str[10];
-    sprintf(puerto_str, "%d", puerto_master);
-    int conexion_master = crear_conexion(ip_master, puerto_str, "WORKER");
+
+int conectar_a_master(){ //TODO:  agregar al header. 
+    int conexion_master = crear_conexion(ip_master, puerto_master, "WORKER");
         if (conexion_master == -1) {
             log_error(logger, "No se pudo conectar a Master en %s:%d", ip_master, puerto_master);
             config_destroy(config);
@@ -43,21 +42,48 @@ void crear_client_worker_master(){
             abort();
         }
         log_info(logger, "Conectado a Master en %s:%d", ip_master, puerto_master);
-    //char * mensaje_a_enviar = strcat("Hola Master mi id es: ", )
-    enviar_mensaje("hola master, mi id es: *inserte id *", conexion_storage);
+        return conexion_master;
+}
+/*
 
-    // Recibir respuesta
-    int cod_op = recibir_operacion(conexion_storage);
-    if (cod_op == MENSAJE) {
-        int size = 0;
-        //char* respuesta = recibir_buffer(&size, conexion_storage);
-        //log_info(logger, "Respuesta del Storage: %s", respuesta);
-        int* respuesta = recibir_buffer(&size, conexion_storage);
-        log_info(logger, "Respuesta del Storage: %d", *respuesta);
-        free(respuesta);
-    } else {
-        log_error(logger, "Operación desconocida recibida: %d", cod_op);
-    }
-     liberar_conexion(conexion_storage);
+void enviar_solicitud_query(int conexion, t_argumentos* args, t_log* logger)
+{
+    log_info(logger, "## Solicitud de ejecución de Query: %s, prioridad: %s", 
+             args->archivo_query, args->prioridad);
+             
+    t_paquete* paquete = crear_paquete();
+    agregar_a_paquete(paquete, args->archivo_query, strlen(args->archivo_query) + 1);
+    agregar_a_paquete(paquete, args->prioridad, strlen(args->prioridad) + 1);
+    
+    enviar_paquete(paquete, conexion);
+    eliminar_paquete(paquete);
+    
+    log_info(logger, "Paquete enviado exitosamente al Master");
+}
 
+*/
+
+void enviar_ID_WORKER_a_master(conexion_master){ // TODO: agregar al header
+    int id_worker = 33; //Completamente hardcodeado mal TODO: Armar la logica de generacion de IDs
+    log_info(logger,"Quiero mandarle al master el ID de este Worker: %d",id_worker);
+    t_paquete* paquete = crear_paquete();
+    agregar_a_paquete(paquete,id_worker,size_of(id_worker));
+    enviar_paquete(paquete,conexion_master);
+    eliminar_paquete(paquete);
+    log_info(logger,"Envie el id al master");
+
+}
+
+
+//Esto es lo que tengo que agregar al MAIN de WORKER, donde tenemos el envío de un mensaje a MASTER
+//En este caso lo que vamos a mandar es el ID del worker, que es lo que pide el check 1 ;)
+void crear_client_worker_master(){ //TODO: Agregar al header
+    int conexion = conectar_a_master();
+    int tipo_cliente = 1; 
+    send(conexion, &tipo_cliente, sizeof(tipo_cliente), 0);
+
+    enviar_ID_WORKER_a_master(conexion);
+
+    liberar_conexion(conexion);
+    log_info(logger, "Conexión con Master cerrada");
 }
--- worker/src/inicializar_estructuras.c
+++ worker/src/inicializar_estructuras.c
@@ -4,9 +4,9 @@ t_log* logger;
 t_config* config;
 
 char * ip_master;
-int puerto_master;
+char* puerto_master;
 char * ip_storage;
-int puerto_storage;
+char* puerto_storage;
 int tam_memoria;
 int retardo_memoria;
 char* algoritmo_reemplazo;
@@ -38,9 +38,9 @@ t_config* iniciar_config(char* path) {
 
 void leer_config(t_config* config){
     ip_master = config_get_string_value(config, "IP_MASTER");
-    puerto_master = config_get_int_value(config, "PUERTO_MASTER");
+    puerto_master = config_get_string_value(config, "PUERTO_MASTER");
     ip_storage = config_get_string_value(config, "IP_STORAGE");
-    puerto_storage = config_get_int_value(config, "PUERTO_STORAGE");
+    puerto_storage = config_get_string_value(config, "PUERTO_STORAGE");
     tam_memoria = config_get_int_value(config, "TAM_MEMORIA");
     retardo_memoria = config_get_int_value(config, "RETARDO_MEMORIA");
     algoritmo_reemplazo = config_get_string_value(config, "ALGORITMO_REEMPLAZO");
