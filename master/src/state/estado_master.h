83c626d4c603e85274ae98eeba0fc397ac5738ed
Raul Sequeira <raulsequeira@frba.utn.edu.ar>
2025-12-01 23:49:40 +0000

    Se define el algoritmo de planificacion segun parametros

--- master/src/state/estado_master.h
+++ master/src/state/estado_master.h
@@ -10,6 +10,11 @@ typedef enum {
     QUERY_EXIT
 } e_estado_query;
 
+typedef enum {
+    ALGO_FIFO,
+    ALGO_PRIORIDAD
+} e_algoritmo_planificacion;
+
 typedef struct {
     int   query_id;
     int   prioridad;
@@ -31,12 +36,15 @@ extern t_list* LISTA_EXEC;                  // Lista de EXEC (cada worker define
 extern pthread_mutex_t MUTEX_QUERY_CONTROL;    // Mutex para proteger la lista de conexiones de Query Control
 extern pthread_mutex_t MUTEX_READY;            // Mutex para proteger COLA_READY (y LISTA_EXEC cuando se use)
 extern int PROXIMO_QUERY_ID;                   // Contador atómico y compartido de IDs de Query
+extern e_algoritmo_planificacion ALGORITMO_ACTUAL; // Algoritmo de planificación activo
 
 void inicializar_estado_master(void);
 void finalizar_estado_master(void);
+void configurar_algoritmo_planificacion(const char* algoritmo_str);
 
 t_query* crear_query(int socket_query_control, const char* path_query, int prioridad);
-void encolar_en_ready(t_query* query);                                      // Encola en READY (FIFO)
+void encolar_en_ready(t_query* query);                                      // Encola según algoritmo configurado
+t_query* obtener_proxima_query_ready(void);                                // Obtiene según algoritmo configurado
 int cantidad_en_ready(void);                                                // Cantidad de elementos en READY (útil para logs)
 void asociar_query_a_conexion(int socket_query_control, t_query* query);   // Asocia la query al dueño (Query Control) para poder limpiarla/identificarla luego
 
